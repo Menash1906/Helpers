0a84bcc6467d650cb9ff00c2431e9bffb8009f56
Menash1906 <Menashyt@gmail.com>
2023-04-23 19:48:05 +0300

    first build

---
+++ Sources/Helpers/Color.swift
@@ -0,0 +1,40 @@
+//
+//  Color.swift
+//  AntiMess
+//
+//  Created by Michael Menashe on 03/04/2023.
+//
+
+import SwiftUI
+
+extension Color {
+#if os(iOS)
+    typealias color = UIColor
+#else
+    typealias color = NSColor
+#endif
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+        
+        let red = Double((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgbValue & 0x0000FF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue)
+    }
+    
+    var hex: String {
+        guard let components = color(self).cgColor.components, components.count >= 3 else {
+            return ""
+        }
+        
+        let r = Float(components[0])
+        let g = Float(components[1])
+        let b = Float(components[2])
+        
+        let hexString = String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+        return hexString
+    }
+}
---
+++ Sources/Helpers/Formatting.swift
@@ -0,0 +1,26 @@
+//
+//  Formatting.swift
+//  AntiMess
+//
+//  Created by Michael Menashe on 08/03/2023.
+//
+
+import Foundation
+
+extension Double {
+    
+    //Expected: 1 Hour/ 2 Hours/ 1 Minute/ 2 Minutes
+    
+    func formatEstimatedTimeToString() -> String {
+        if self >= 60 {
+            if (Int(self) % 60) != 0 {
+                return "\(Int(self / 60))H \(Int(self) % 60)M"
+            } else {
+                return "\(Int(self) / 60) Hour\(self == 60 ? "" : "s")"
+            }
+        } else {
+            let string = "\(Int(self)) Minute\(self == 60 ? "" : "s")"
+            return string
+        }
+    }
+}
